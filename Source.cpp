2d68c53b131d9b0c29a35adc630fdc1ddbf43c1a
TehminaSaleem <91927385+TehminaSaleem@users.noreply.github.com>
2021-10-10 10:56:34 +0500

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -3,29 +3,23 @@ using namespace std;
 int main() {
 	float BMI;
 	float weight;
-	float height=60;
-	float underweight;
-	float normal;
-	float overweight;
-	float obese;
+	float height;
 	cout << "\nEnter height (in inches):";
-	cin >> weight;
+	cin >> height;
 	cout << "\nEnter weight (in pounds):";
 	cin >> weight;
 	BMI = (703*weight)/(height*height);
-	//cout << "\nBMI is:" << BMI;
 	if (BMI <= 18.5) {
 		cout << "\nUnderweight";
 	}
 	if (BMI >= 18.5 && BMI <= 24.9){
 		cout << "\nNormal";
 	}
-	
-		if (BMI >= 25.0 && BMI <= 29.9){
-			cout << "\nOverweight";
+	if (BMI >= 25.0 && BMI <= 29.9){
+		cout << "\nOverweight";
 	}
-	
-		if (BMI >= 30){
-			cout << "\nObese"; }
+	if (BMI >= 30){
+		cout << "\nObese"; 
+	}
 	return 0;
 }
